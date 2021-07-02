12af38d31f3049b4ddb87dd89fc98c2c8e217d5d
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-07-01 21:34:45 -0500

    adding portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,17 @@ modalCloses.forEach((modalClose) =>{
         })
     })
 })
+
+/*portfolio swiper*/
+let swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
