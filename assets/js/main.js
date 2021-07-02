c567558d2c1416e649d5c7053bddce8bd9c19754
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-07-02 17:15:27 -0500

    fixing testimonal next back

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,17 @@ let swiper = new Swiper('.portfolio__container', {
       clickable: true,
     }
   });
+
+  /*testimonial swiper*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
