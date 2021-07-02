61deb1343ee9f414fb194760e92103adf030b57b
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-07-02 16:25:36 -0500

    adding project in mind

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,13 +104,13 @@ modalCloses.forEach((modalClose) =>{
 /*portfolio swiper*/
 let swiper = new Swiper('.swiper-container', {
     cssMode: true,
+    loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     pagination: {
-      el: '.swiper-pagination'
-    },
-    mousewheel: true,
-    keyboard: true,
+      el: '.swiper-pagination',
+      clickable: true,
+    }
   });
