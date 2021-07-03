d000c22a93016580c5adce013253189f9ed1752a
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-07-03 13:44:10 -0500

    fixing bug scrollHeader

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,3 +134,35 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
     }
   });
 
+/*scroll sections active link*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            console.log('valor')
+            console.log(sectionId)
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header_scroll_val')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); 
+    else nav.classList.remove('scroll-header')
+    
+}
+window.addEventListener('scroll', scrollHeader)
